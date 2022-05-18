22ae8acbfcaae4908cfd8b71ed753b62bfc57419
Drake Dev <thanhlong9123@gmail.com>
2022-05-19 00:43:48 +0700

    response with user info

--- routes/auth.js
+++ routes/auth.js
@@ -5,81 +5,82 @@ const jwt = require("jsonwebtoken");
 
 // REGISTER
 router.post("/register", async (req, res) => {
-  try {
-    const userExistence = await User.findOne({
-      $or: [{ username: req.body.username }, { email: req.body.email }],
-    });
+	try {
+		const userExistence = await User.findOne({
+			$or: [{ username: req.body.username }, { email: req.body.email }],
+		});
 
-    if (userExistence) {
-      return res.status(400).json({
-        error: "Username or email already exists",
-      });
-    }
+		if (userExistence) {
+			return res.status(400).json({
+				error: "Username or email already exists",
+			});
+		}
 
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.PASS_SEC
-      ).toString(),
+		const newUser = new User({
+			username: req.body.username,
+			email: req.body.email,
+			password: CryptoJS.AES.encrypt(
+				req.body.password,
+				process.env.PASS_SEC
+			).toString(),
 
-      isAdmin: req.body.isAdmin,
-    });
+			isAdmin: req.body.isAdmin,
+		});
 
-    const savedUser = await newUser.save();
-    res.status(201).json({
-      message: "User created successfully",
-    });
-  } catch (err) {
-    res.status(500).json({
-      error: err,
-    });
-  }
+		const savedUser = await newUser.save();
+		res.status(201).json({
+			message: "User created successfully",
+			savedUser,
+		});
+	} catch (err) {
+		res.status(500).json({
+			error: err,
+		});
+	}
 });
 
 // LOGIN
 router.post("/login", async (req, res, next) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
+	try {
+		const user = await User.findOne({ username: req.body.username });
 
-    if (!user) {
-      return res.status(401).json({
-        error: "Username or password is incorrect",
-      });
-    }
+		if (!user) {
+			return res.status(401).json({
+				error: "Username or password is incorrect",
+			});
+		}
 
-    const hashedPassword = CryptoJS.AES.decrypt(
-      user.password,
-      process.env.PASS_SEC
-    );
+		const hashedPassword = CryptoJS.AES.decrypt(
+			user.password,
+			process.env.PASS_SEC
+		);
 
-    const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+		const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
-    if (originalPassword !== req.body.password) {
-      return res.status(401).json({ error: "Wrong credentials" });
-    }
+		if (originalPassword !== req.body.password) {
+			return res.status(401).json({ error: "Wrong credentials" });
+		}
 
-    // Using JWT
-    const accessToken = jwt.sign(
-      {
-        id: user._id,
-        isAdmin: user.isAdmin,
-      },
-      process.env.JWT_SEC,
+		// Using JWT
+		const accessToken = jwt.sign(
+			{
+				id: user._id,
+				isAdmin: user.isAdmin,
+			},
+			process.env.JWT_SEC,
 
-      // User have 3 day to use token to access
-      { expiresIn: "3d" }
-    );
+			// User have 3 day to use token to access
+			{ expiresIn: "3d" }
+		);
 
-    const { password, ...others } = user._doc;
+		const { password, ...others } = user._doc;
 
-    res.status(200).json({ ...others, accessToken });
-  } catch (err) {
-    console.log(err);
-    // res.status(500).json(err);
-    next(err);
-  }
+		res.status(200).json({ ...others, accessToken });
+	} catch (err) {
+		console.log(err);
+		// res.status(500).json(err);
+		next(err);
+	}
 });
 
 module.exports = router;
